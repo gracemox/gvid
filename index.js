dbc4f33829d1a48fa358311a579a69efd5c19b91
Grace Mox <grace.e.mox@gmail.com>
2016-01-16 17:10:25 -0500

    added function to extract url from form input

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 function videoIdFromUrl(url){ return url.slice(-11); };
 
 function buildIframe(videoId){
-  var beginning = '<iframe src="https://www.youtube.com/embed/';
-  var end = '" frameborder="0" allowfullscreen></iframe>';
-  var iframe = beginning + videoId + end;
-  return iframe;
+	var beginning = '<iframe src="https://www.youtube.com/embed/';
+	var end = '" frameborder="0" allowfullscreen></iframe>';
+	var iframe = beginning + videoId + end;
+	return iframe;
 };
+
+function urlFromForm(){
+	return $('input').val();
+}
 
 function addVideo(){
 	// extract video id from url
 	// built iframe html string
 	// add iframe string to the page
-	var url = 'https://www.youtube.com/watch?v=v3kh4HVSqWQ';
+	var url = urlFromForm();
 	var videoId = videoIdFromUrl(url);
 	var iframe = buildIframe(videoId);
 	$('.flex-parent').append(iframe);
